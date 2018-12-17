e8a0943cacbc9392aa417160bffdde1a1312d066
DAppDevelop <chenysh521@163.com>
2018-12-17 23:15:18 +0800

    私链添加智能合约；metamask 连接私链的时候要注意设置chanel ID 查询账户token余额

--- controllers/account.js
+++ controllers/account.js
@@ -1,6 +1,7 @@
 let web3 = require("../utils/myUtils").getweb3()
 let {success, fail} = require("../utils/myUtils")
 let fs = require("fs")
+let myContract = require("../models/contract").getContract()
 
 async function getAccountBalance(address) {
     let balance = await web3.eth.getBalance(address)
@@ -14,18 +15,18 @@ async function setResponseData(account) {
     // console.log(balance)
 
     //获取代币的数据
-    // let myBalance = await myContract.methods.balanceOf(account.address).call()
-    // let decimals = await myContract.methods.decimals().call()
-    // myBalance = myBalance / Math.pow(10, decimals)
-    // let symbol = await myContract.methods.symbol().call()
+    let myBalance = await myContract.methods.balanceOf(account.address).call()
+    let decimals = await myContract.methods.decimals().call()
+    myBalance = myBalance / Math.pow(10, decimals)
+    let symbol = await myContract.methods.symbol().call()
 
     //返回相应数据给前端
     return success({
         balance: balance,
         address: account.address,
         privatekey: account.privateKey,
-        // tokenbalance: myBalance,
-        // symbol: symbol
+        tokenbalance: myBalance,
+        symbol: symbol
     })
 }
 
